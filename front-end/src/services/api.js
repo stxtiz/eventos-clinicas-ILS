3c3c45fa237050742cd6d66261f45f28c810b9e4
Cirayt <carlos0tapi@gmail.com>
2025-11-03 12:36:48 -0400

    hola

--- front-end/src/services/api.js
+++ front-end/src/services/api.js
@@ -7,7 +7,7 @@
 
 */
 const API_BASE_URL =
-  import.meta.env.PUBLIC_API_URL || "http://localhost:3000/api";
+  import.meta.env.PUBLIC_API_URL || "http://eventos-clinicas-ILS.test/api";
 
 export class ApiService {
   static async request(endpoint, options = {}) {
